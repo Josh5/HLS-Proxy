c5616cd9b70a6c85695764a498c426e33ad5ebab
Josh.5 <jsunnex@gmail.com>
2024-11-03 23:35:16 +1300

    Re-write proxy
    Use only base64 encoded paths and keep it simple

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import asyncio
+import os
+
 from backend import create_app, enable_debugging
 
 # Create app
@@ -8,6 +11,10 @@ if enable_debugging:
     app.logger.info(' DEBUGGING   = ' + str(enable_debugging))
 
 if __name__ == "__main__":
+    # Create a custom loop
+    loop = asyncio.get_event_loop()
+
     # Start Quart server
     app.logger.info("Starting Quart server...")
-    app.run(debug=True, host='0.0.0.0', port=9987)
+    app.run(loop=loop, debug=True, host='0.0.0.0', port=os.environ.get('HLS_PROXY_PORT', 9987))
+    app.logger.info("Quart server completed.")
